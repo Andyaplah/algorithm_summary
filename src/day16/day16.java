24f7a1e37954803bf385032cd7fc52eae292c77a
Andyaplah <124140801@qq.com>
2020-06-23 00:21:50 +0800

    day22

---
+++ src/day16/day16.java
@@ -0,0 +1,9 @@
+package day16;
+
+public class day16{
+
+
+    public static void main(String[] args) {
+        System.out.println("---------------");
+    }
+}
--- src/day17/day_17_leetcode232.c
+++ src/day17/day_17_leetcode232.c
@@ -21,10 +21,6 @@ typedef struct {
 /** Initialize your data structure here. */
 
 MyQueue* myQueueCreate() {
-//    MyQueue *TempQueue = malloc(sizeof(MyQueue));
-//    TempQueue->mystack1.TOP -1;
-//    TempQueue->mystack2.TOP -1;
-//    return TempQueue;
     MyQueue* TempQueue=(MyQueue*)malloc(sizeof(MyQueue));
     TempQueue->mystack1.TOP=-1;
     TempQueue->mystack2.TOP=-1;
@@ -43,38 +39,48 @@ void myQueuePush(MyQueue* obj, int x) {
 
     if(obj->mystack1.TOP<MAX_SIZE){ //栈是否满
         while(obj->mystack1.TOP!=-1){
-            obj->mystack2.DATA[++(obj->mystack2.TOP)]=obj->mystack1.DATA[(obj->mystack1.TOP)--]; //把S1栈中元素压入S2实现反转
+            obj->mystack2.DATA[++(obj->mystack2.TOP)]=obj->mystack1.DATA[(obj->mystack1.TOP)--];
         }
-        obj->mystack1.DATA[++(obj->mystack1.TOP)]=x; //把push的元素压入S1栈（此时S1为空栈，因为它的元素已经全部给S2啦）
+        obj->mystack1.DATA[++(obj->mystack1.TOP)]=x;
         while(obj->mystack2.TOP!=-1){
-            obj->mystack1.DATA[++(obj->mystack1.TOP)]=obj->mystack2.DATA[(obj->mystack2.TOP)--]; //再把S2栈中的元素全部反转压入S1
+            obj->mystack1.DATA[++(obj->mystack1.TOP)]=obj->mystack2.DATA[(obj->mystack2.TOP)--];
         }
     }
 }
 
 /** Removes the element from in front of queue and returns that element. */
 int myQueuePop(MyQueue* obj) {
-    if(obj->mystack2.TOP == -1){
-        while (obj->mystack1.TOP= -1){
-            obj->mystack2.DATA[++(obj->mystack2.TOP)] = obj->mystack1.DATA[--(obj->mystack1.TOP)];
-        }
-        return obj->mystack2.DATA[(obj->mystack2.TOP--)];
-    }
+//    if(obj->mystack2.TOP == -1){
+//        while (obj->mystack1.TOP= -1){
+//            obj->mystack2.DATA[++(obj->mystack2.TOP)] = obj->mystack1.DATA[--(obj->mystack1.TOP)];
+//        }
+//        return obj->mystack2.DATA[(obj->mystack2.TOP--)];
+//    }
+if(obj->mystack1.TOP!=-1)
+        return obj->mystack1.DATA[(obj->mystack1.TOP)--];
+    return NULL;
+
 }
 
 /** Get the front element. */
 int myQueuePeek(MyQueue* obj) {
-    if(obj->mystack2.TOP == -1){
-        while (obj->mystack1.TOP= -1){
-            obj->mystack2.DATA[++(obj->mystack2.TOP)] = obj->mystack1.DATA[--(obj->mystack1.TOP)];
-        }
-        return obj->mystack2.DATA[(obj->mystack2.TOP)];
-    }
+//    if(obj->mystack2.TOP == -1){
+//        while (obj->mystack1.TOP= -1){
+//            obj->mystack2.DATA[++(obj->mystack2.TOP)] = obj->mystack1.DATA[--(obj->mystack1.TOP)];
+//        }
+//        return obj->mystack2.DATA[(obj->mystack2.TOP)];
+//    }
+if(obj->mystack1.TOP!=-1)
+        return obj->mystack1.DATA[obj->mystack1.TOP];
+    return NULL;
+
 }
 
 /** Returns whether the queue is empty. */
 bool myQueueEmpty(MyQueue* obj) {
-    return obj->mystack1.TOP=-1 && obj->mystack2.TOP==-1;
+     if(obj->mystack1.TOP==-1)
+            return true;
+        return false;
 }
 
 void myQueueFree(MyQueue* obj) {
