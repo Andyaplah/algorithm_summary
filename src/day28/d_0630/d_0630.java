fcdac4fb8385b570463ff6db9fea2d399ccbd144
Andyaplah <linchuanxin2007@163.com>
2020-07-01 14:33:19 +0800

    day29-31 java kotlin c

---
+++ src/day28/d_0630/d_0630.java
@@ -0,0 +1,25 @@
+package day28.d_0630;
+
+public class d_0630 {
+
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int standSum = k * threshold;
+        int count = 0,sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        if(sum >= standSum)
+            count = 1;
+
+        for (int j = k; j < arr.length; j++) {
+            sum -= arr[j-k];
+            sum += arr[j];
+            if (sum >= standSum)
+                count++;
+        }
+
+        return count;
+    }
+}
